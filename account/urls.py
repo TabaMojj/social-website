2c22c7633f50c5575ef057a94c752a798a738d92
Mojtaba Mir Yaghoobzdeh <mirmojtaba21@gmail.com>
2023-07-18 14:40:58 +0330

    Remove redundant paths. Add django auth_views

--- account/urls.py
+++ account/urls.py
@@ -1,10 +1,6 @@
-from django.urls import path
-from django.contrib.auth import views as auth_views
-from . import views
+from django.urls import path, include
+
 
 urlpatterns = [
-    path('login/', auth_views.LoginView.as_view(), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('', views.dashboard, name='dashboard')
-
+    path('', include('django.contrib.auth.urls')),
 ]
