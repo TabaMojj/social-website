8c0abe71ce2e56a79c2259c6f69120bbbe07cf10
Mojtaba Mir Yaghoobzdeh <mirmojtaba21@gmail.com>
2023-07-19 11:08:34 +0330

    Fix bug in getting extension

--- images/form.py
+++ images/form.py
@@ -23,7 +23,7 @@ class ImageCreateForm(forms.ModelForm):
         image = super().save(commit=False)
         image_url = self.cleaned_data['url']
         name = slugify(image.title)
-        extension = image_url.split('.', 1)[1].lower()
+        extension = image_url.rsplit('.', 1)[1].lower()
         image_name = f'{name}.{extension}'
         response = requests.get(image_url)
         image.image.save(image_name, ContentFile(response.content), save=False)
