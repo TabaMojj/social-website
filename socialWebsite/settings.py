ccba033cfd251c2d1552a3ce1a6ee90bfcdda417
Mojtaba Mir Yaghoobzdeh <mirmojtaba21@gmail.com>
2023-07-25 21:42:09 +0300

    feat: add debug-toolbar

--- socialWebsite/settings.py
+++ socialWebsite/settings.py
@@ -30,10 +30,12 @@ INSTALLED_APPS = [
     'images.apps.ImagesConfig',
     'django_extensions',
     'easy_thumbnails',
-    'actions.apps.ActionsConfig'
+    'actions.apps.ActionsConfig',
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -121,3 +123,7 @@ ABSOLUTE_URL_OVERRIDES = {
     'auth.user': lambda u: reverse_lazy('user_detail',
                                         args=[u.username])
 }
+
+INTERNAL_IPS = [
+    '127.0.0.1',
+]
