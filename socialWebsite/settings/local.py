b4bf5496da641d0040ce4c244ae62729808b6269
Mojtaba Mir Yaghoobzdeh <mirmojtaba21@gmail.com>
2023-09-04 16:18:00 +0330

    feat: add different settings for local and production environments

---
+++ socialWebsite/settings/local.py
@@ -0,0 +1,16 @@
+from .base import *
+
+DEBUG = True
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+REDIS_HOST = 'localhost'
+REDIS_PORT = 6379
+REDIS_DB = 0
